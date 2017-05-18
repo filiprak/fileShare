5928619514bec3730d8b33ad5aa15aa1ff5f5681
Julita Ołtusek <julita.oltusek@gmail.com>
2017-05-18 17:13:34 +0200

    Add comment to controller

--- src/logic/controller.cpp
+++ src/logic/controller.cpp
@@ -1,11 +1,15 @@
 /*
  * controller.cpp
  *
+ * Controls connection between components.
+ *
  *  Created on: 17.05.2017
  *      Author: raqu
  */
 
 #include "controller.h"
+
+
 
 Controller::Controller() {
 	// TODO Auto-generated constructor stub
