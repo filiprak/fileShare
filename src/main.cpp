be57d5847273496677917b14b21768a6c7a5be10
filiprak <filiprak@wp.pl>
2017-05-20 15:07:52 +0200

    Change blocking queue to fixed size queue

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,39 @@
  */
 
 #include <iostream>
+#include <thread>
+#include <blockingQueue.h>
+
+#include <cstdlib>
+#include <cstdio>
+#include <ctime>
+#include <unistd.h>
+
+void producent(BlockingQueue<int>* q) {
+	for (int i = 0; i < 10; ++i) {
+			unsigned int t = rand()%3;
+			for (int j=0; j<3; ++j) {
+				int el = rand()%20 + 10000;
+				q->push(el);
+			}
+			sleep(t);
+		}
+}
 
 int main() {
-	std::cout << "hello world !" << std::endl;
+	srand( time( NULL ) );
+
+	BlockingQueue<int> queue(2, false);
+
+	std::thread prodthread(producent, &queue);
+
+	for (int i = 0; i < 10; ++i) {
+		int elem = queue.pop();
+	}
+
+	prodthread.join();
+
 	return 0;
 }
+
 
