b5e6e9e11550444929f9dfe30ddffd78253013dd
filiprak <filiprak@wp.pl>
2017-05-27 16:22:33 +0200

    Add Console UI class sources

---
+++ include/console.h
@@ -0,0 +1,48 @@
+/*
+ * console.h
+ *
+ *  Created on: 27.05.2017
+ *      Author: raqu
+ */
+
+#ifndef UTILS_CONSOLE_H_
+#define UTILS_CONSOLE_H_
+
+#include <curses.h>
+#include <mutex>
+#include <controller.h>
+
+
+class Console {
+private:
+	std::mutex mux;
+	Controller* controller = nullptr;
+	WINDOW* inputLine;
+	WINDOW* outputLines;
+	bool running = false;
+
+public:
+	Console();
+	virtual ~Console();
+
+	void sendFormattedMsg(short prefixColor,
+			const char* prefix, short color, const char* format, ...);
+	void inputLoop(void);
+
+	bool isRunning() const {
+		return running;
+	}
+
+	void stop() {
+		running = false;
+	}
+
+	void setController(Controller* controller) {
+		this->controller = controller;
+	}
+};
+
+//create user interface
+extern Console UI;
+
+#endif /* UTILS_CONSOLE_H_ */
---
+++ src/utils/console.cpp
@@ -0,0 +1,119 @@
+/*
+ * console.cpp
+ *
+ *  Created on: 27.05.2017
+ *      Author: raqu
+ */
+
+#include "console.h"
+#include "ncurses.h"
+#include "logger.h"
+#include <string>
+#include "errno.h"
+
+Console UI;
+
+Console::Console() {
+	initscr();
+
+	inputLine = newwin(1, COLS, LINES - 1, 0);
+	outputLines = newwin(LINES - 1, COLS, 0, 0);
+
+	if (has_colors()) {
+		start_color();
+		for (int i = 1; i <= COLOR_WHITE; ++i) {
+			init_pair(i, i, COLOR_BLACK);
+		}
+	} else
+		wprintw(outputLines, "Terminal cannot print colors.\n");
+
+	scrollok(outputLines, TRUE);
+	scrollok(inputLine, TRUE);
+
+	leaveok(inputLine, TRUE);
+	nodelay(inputLine, FALSE);
+	cbreak();
+	noecho();
+	keypad(inputLine, TRUE);
+	// hide cursor
+	curs_set(0);
+
+	running = true;
+}
+
+void Console::sendFormattedMsg(short prefixColor,
+		const char* prefix, short color, const char* format, ...) {
+	mux.lock();
+    va_list args;
+    va_start(args, format);
+
+	if (has_colors()) {
+		if (prefix) {
+			wattron(outputLines, A_BOLD | COLOR_PAIR(prefixColor));
+			wprintw(outputLines, prefix);
+		}
+
+		if (color == COLOR_WHITE)
+			wattroff(outputLines, A_BOLD);
+		wattron(outputLines, COLOR_PAIR(color));
+		vwprintw(outputLines, format, args);
+
+		wattroff(outputLines, A_BOLD | COLOR_PAIR(color));
+	} else {
+		wprintw(outputLines, prefix);
+		vwprintw(outputLines, format, args);
+	}
+
+    wrefresh(outputLines);
+    va_end(args);
+    mux.unlock();
+}
+
+void Console::inputLoop(void) {
+
+    static std::string input;
+
+    mux.lock();
+    wattron(inputLine, A_BOLD | COLOR_PAIR(COLOR_WHITE));
+    wprintw(inputLine, "\n>: ");
+    wattroff(inputLine, A_BOLD | COLOR_PAIR(COLOR_WHITE));
+
+    wprintw(inputLine, input.c_str());
+    wrefresh(inputLine);
+    mux.unlock();
+
+    int c = wgetch(inputLine);
+
+    if (c == ERR) {
+    	logger->warn("Input character read error");
+        return;
+    }
+
+    switch (c) {
+		case '\n':
+			if (input.size() > 0) {
+				sendFormattedMsg(COLOR_WHITE, ">: ", COLOR_WHITE, input.c_str());
+
+				wprintw(inputLine, "%s\n", input);
+				if (input == "q") running = false;
+				input.clear();
+			}
+			break;
+
+		case KEY_DC:
+		case KEY_BACKSPACE:
+			if (input.size() > 0) input.pop_back();
+			break;
+
+		default:
+			input += (char) c;
+			break;
+    }
+}
+
+Console::~Console() {
+	delwin(inputLine);
+	delwin(outputLines);
+	endwin();
+}
+
